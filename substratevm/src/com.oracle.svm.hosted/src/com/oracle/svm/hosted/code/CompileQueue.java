7f06e66b946426b14d65ab69286ca82523198bd7
Paul Wögerer <paul.woegerer@oracle.com>
2022-06-23 15:36:17 +0200

    Externalize Comparability of Hosted{Type,Method,Field}

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CompileQueue.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CompileQueue.java
@@ -179,6 +179,7 @@ import com.oracle.svm.hosted.meta.HostedField;
 import com.oracle.svm.hosted.meta.HostedMethod;
 import com.oracle.svm.hosted.meta.HostedType;
 import com.oracle.svm.hosted.meta.HostedUniverse;
+import com.oracle.svm.hosted.meta.UniverseBuilder;
 import com.oracle.svm.hosted.phases.DevirtualizeCallsPhase;
 import com.oracle.svm.hosted.phases.HostedGraphBuilderPhase;
 import com.oracle.svm.hosted.phases.ImageBuildStatisticsCounterPhase;
@@ -1659,7 +1660,7 @@ public class CompileQueue {
     }
 
     public Map<HostedMethod, CompilationResult> getCompilationResults() {
-        Map<HostedMethod, CompilationResult> result = new TreeMap<>();
+        Map<HostedMethod, CompilationResult> result = new TreeMap<>(UniverseBuilder.getMethodComparator());
         for (Entry<HostedMethod, CompileTask> entry : compilations.entrySet()) {
             result.put(entry.getKey(), entry.getValue().result);
         }
